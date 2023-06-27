bdf3ac0614aa02c4ed6df6b4bb173f43786a0d94
jemma-mg <jemmamariyageorge@gmail.com>
2023-06-27 23:43:34 +0530

    question 2 initial

---
+++ q2.c
@@ -0,0 +1,8 @@
+/*Define a structure that describes a hotel with name, grade and average room rent as members. 
+Write function to print out hotels of a given grade.*/
+
+#include <stdio.h>
+
+void main(){
+    printf("Hotel listing using structure");
+}
